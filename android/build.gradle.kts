a6e7d2780b01f36479bb803d2a7ca3ca288d5a17
o@neepo.dev <o@neepo.dev>
2025-12-19 00:19:23 +0300

    Add settings according to vkid_flutter_sdk guide.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,3 +15,24 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+plugins {
+    id("vkid.manifest.placeholders") version "1.1.0" apply true
+}
+
+val localProperties = java.util.Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localProperties.load(localPropertiesFile.reader())
+}
+
+vkidManifestPlaceholders {
+    init(
+        clientId = "54294664",
+        clientSecret = localProperties["VKID_CLIENT_SECRET"] as String,
+    )
+    vkidRedirectHost = "vk.ru"
+    vkidRedirectScheme = "vk54294664"
+    vkidClientId = "54294664"
+    vkidClientSecret = localProperties["VKID_CLIENT_SECRET"] as String
+}
